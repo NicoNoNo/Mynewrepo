7daf7fcdf159528f0497e4efa43d3bc6ed12e6c3
Nicolaj <Nicolaj@Nicolajs-MBP.lan>
2020-10-27 18:15:34 +0100

    Hey

---
+++ Timexp.java
@@ -0,0 +1,18 @@
+//Converter hours and min and sec into only sec
+public class Timexp{
+public static void main(String[]args){
+    int hours= 11;
+    int min= 45;
+    int sec= 32;
+    
+    int h= (hours*60*60);
+    int m= (min*60);
+    
+    System.out.println(h+m+sec);
+    
+    
+    
+}
+
+
+}
